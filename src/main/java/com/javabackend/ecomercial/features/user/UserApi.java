7e532d87e4a1f4a56074427c299e375d5f07114e
Tuan Nguyen <tuanquangnguyen1710@gmail.com>
2022-02-20 10:29:53 +0200

    ECM-1001: Exchange service from controller to serviceimpl.

--- src/main/java/com/javabackend/ecomercial/features/user/UserApi.java
+++ src/main/java/com/javabackend/ecomercial/features/user/UserApi.java
@@ -1,69 +1,36 @@
-package com.javabackend.ecomercial.api;
+package com.javabackend.ecomercial.features.user;
 
 import com.javabackend.ecomercial.domain.entity.RefreshToken;
 import com.javabackend.ecomercial.exception.TokenRefreshException;
-import com.javabackend.ecomercial.features.user.UserResponse;
+import com.javabackend.ecomercial.payload.request.LogOutRequest;
 import com.javabackend.ecomercial.payload.request.LoginRequest;
 import com.javabackend.ecomercial.payload.request.SignupRequest;
 import com.javabackend.ecomercial.payload.request.TokenRefreshRequest;
 import com.javabackend.ecomercial.payload.response.JwtResponse;
 import com.javabackend.ecomercial.payload.response.MessageResponse;
 import com.javabackend.ecomercial.payload.response.TokenRefreshResponse;
-import com.javabackend.ecomercial.repository.RoleRepository;
-import com.javabackend.ecomercial.repository.UserRepository;
 import com.javabackend.ecomercial.security.jwt.JwtUtils;
 import com.javabackend.ecomercial.security.services.RefreshTokenService;
-import com.javabackend.ecomercial.security.services.UserDetailsImpl;
-import com.javabackend.ecomercial.security.services.UserDetailsServiceImpl;
 import com.javabackend.ecomercial.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 @AllArgsConstructor
-public class AuthController {
-
-    private final AuthenticationManager authenticationManager;
-    private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
-    private final PasswordEncoder encoder;
+public class UserApi {
     private final JwtUtils jwtUtils;
     private final RefreshTokenService refreshTokenService;
     private final UserService userService;
-    private final UserDetailsServiceImpl userDetailsService;
-
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-
-        Authentication authentication = authenticationManager
-                .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-
-        String jwt = jwtUtils.generateToken(userDetails.getUsername());
-
-        List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority())
-                .collect(Collectors.toList());
-
-        RefreshToken refreshToken = refreshTokenService.createRefreshToken(userDetails.getId());
-
-        return ResponseEntity.ok(new JwtResponse(jwt, refreshToken.getToken(), userDetails.getId(),
-                userDetails.getUsername(), userDetails.getEmail(), roles));
+        JwtResponse jwtResponse = userService.signIn(loginRequest);
+        return ResponseEntity.ok(jwtResponse);
     }
 
     @PostMapping("/refreshtoken")
@@ -84,5 +51,11 @@ public class AuthController {
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
         UserResponse userResponse = userService.registerUser(signUpRequest);
         return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logoutUser(@Valid @RequestBody LogOutRequest logOutRequest) {
+        refreshTokenService.deleteByUserId(logOutRequest.getUserId());
+        return ResponseEntity.ok(new MessageResponse("Log out successful!"));
     }
 }
